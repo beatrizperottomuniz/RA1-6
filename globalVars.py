8eb458c8b0f5069a7d23fadb6d619fb47b4c7884
beatrizperottomuniz <beatrizperottomuniz@gmail.com>
2026-03-22 21:20:05 -0300

    String pool e variaveis globais
    Primeira versao de string pool ; variaveis globais : string pool geral e contador de linhas

---
+++ globalVars.py
@@ -0,0 +1,4 @@
+from stringPool import StringPool
+
+string_pool_global = StringPool()
+line_count_global = 1
---
+++ stringPool.py
@@ -0,0 +1,22 @@
+# para ver se a string (variavel) ja esta na pool
+class StringPool:
+    def __init__(self):
+        self.pool = {} # dicionario string = id
+        self.strings = [] # lista de strings
+
+    # retorna id se existe/ cria novo se não
+    def get_or_add(self, lexeme: str):
+        if lexeme in self.pool:
+            return self.pool[lexeme]
+
+        next_id = len(self.strings) # id é o índice na lista
+        self.pool[lexeme] = next_id
+        self.strings.append(lexeme)
+
+        return next_id
+
+    # retorna texto original de id
+    def get_string(self, symbol_id: int):
+        if 0 <= symbol_id < len(self.strings):
+            return self.strings[symbol_id]
+        return "unknown"
