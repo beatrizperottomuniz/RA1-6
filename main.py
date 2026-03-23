f1a9272632212f4d42e476dd5d03c9d16a375930
beatrizperottomuniz <beatrizperottomuniz@gmail.com>
2026-03-23 11:47:48 -0300

    Funcao para exportar tokens
    Funcao de exportacao de tokens depois de parse para próxima fase
    
    Formato : json

--- main.py
+++ main.py
@@ -3,10 +3,36 @@ Aluna : Beatriz Perotto Muniz
 Grupo : RA1-6
 '''
 import sys
+import json
 from leituraArquivo import lerArquivo
 from analisadorLexico import parseExpressao
 import globalVars
 
+def exportTokens(list_tokens, path_output="saida_tokens.txt"):
+    serialized_tokens = []
+    # token -> dicionario
+    for token in list_tokens:
+        serialized_tokens.append({
+            "type": token.type,
+            "line": token.line,
+            "column": token.column,
+            "symbol_id": token.symbol_id
+        })
+    # tokens + string pool
+    data = {
+        "string_pool": globalVars.string_pool_global.strings,
+        "tokens": serialized_tokens
+    }
+
+    try:
+        with open(path_output, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+        print(f"\nTokens exportados para o arquivo: '{path_output}'")
+    except Exception as e:
+        print(f"\nFalha ao salvar o arquivo de tokens: {e}")
+
+
+# ------------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Uso: python teste_lerArquivo.py <nome_do_arquivo>")
@@ -19,5 +45,7 @@ if __name__ == "__main__":
     for line in lines:
         print(line)
         parseExpressao(line, tokens_list)
-        print(tokens_list)
         globalVars.line_count_global += 1
+        
+    print(tokens_list)
+    exportTokens(tokens_list)
